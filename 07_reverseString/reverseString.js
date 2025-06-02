7c74f267c0167a89e29227821d76ecb4f97fb035
Alexander Brost <52391800+brost2000@users.noreply.github.com>
2025-06-02 11:37:22 +0200

    5-7 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringArray = string.split("");
+  let reverseArray = stringArray.reverse();
+  let stringReversed = reverseArray.join("");
 
+  return stringReversed;
 };
 
 // Do not edit below this line
